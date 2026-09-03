b067249c84f964694b569cafc099da606fc04232
Misba fathima <misbafathimashaik20@gmail.com>
2026-09-03 22:36:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,41 @@
+import streamlit as st
+from resume_parser import extract_resume_text
 
+
+st.set_page_config(
+    page_title="AI Resume Analyzer",
+    page_icon="🤖",
+    layout="centered"
+)
+
+
+st.title("🤖 AI Resume Analyzer")
+
+st.write(
+    "Upload your resume and let the system analyze it."
+)
+
+
+uploaded_file = st.file_uploader(
+    "📄 Upload your Resume",
+    type=["pdf", "docx"]
+)
+
+
+if uploaded_file is not None:
+
+    st.success("Resume uploaded successfully! ✅")
+
+    if st.button("🔍 Analyze Resume"):
+
+        resume_text = extract_resume_text(uploaded_file)
+
+        st.subheader("📄 Extracted Resume Text")
+
+        st.text_area(
+            "Resume Content",
+            resume_text,
+            height=400
+        )
+
+        st.success("Resume text extracted successfully! 🎉")
