316cc9887030a4a3bdc1b125086720bd4aea32de
Misba fathima <misbafathimashaik20@gmail.com>
2026-09-03 22:35:31 +0530

    Update resume_parser.py

--- resume_parser.py
+++ resume_parser.py
@@ -1,1 +1,39 @@
+import fitz
+from docx import Document
 
+
+def extract_pdf_text(file):
+    text = ""
+
+    pdf = fitz.open(stream=file.read(), filetype="pdf")
+
+    for page in pdf:
+        text += page.get_text()
+
+    pdf.close()
+
+    return text
+
+
+def extract_docx_text(file):
+    document = Document(file)
+
+    text = ""
+
+    for paragraph in document.paragraphs:
+        text += paragraph.text + "\n"
+
+    return text
+
+
+def extract_resume_text(file):
+    file_name = file.name.lower()
+
+    if file_name.endswith(".pdf"):
+        return extract_pdf_text(file)
+
+    elif file_name.endswith(".docx"):
+        return extract_docx_text(file)
+
+    else:
+        return "Unsupported file format."
